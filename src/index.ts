a13c540d692bc46a33ed978362da59cb19e2a6b1
hoanggminh2702 <hoanggminh2702@gmail.com>
2024-10-08 08:31:26 +0700

    add useToggle

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { useDebounce, useDebounceFn, useDebounceState } from './hooks/useDebounce'
+import useDidUpdate from './hooks/useDidUpdate'
+import useMount from './hooks/useMount'
+import useToggle from './hooks/useToggle'
+
+export { useDebounce, useDebounceFn, useDebounceState, useDidUpdate, useMount, useToggle }
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import useDidUpdate from './hooks/useDidUpdate'
-
-import useMount from './hooks/useMount'
-
-import { useDebounce, useDebounceFn, useDebounceState } from './hooks/useDebounce'
-
-export { useDebounce, useDebounceFn, useDebounceState, useDidUpdate, useMount }
