b1245ec4e71537ddfc71ba76fd83470820baeb66
hoanggminh2702 <hoanggminh2702@gmail.com>
2024-11-13 23:11:04 +0700

    remove unneccesary event hooks

--- src/hooks/useDidUpdate.ts
+++ src/hooks/useDidUpdate.ts
@@ -1,28 +1,10 @@
-import { DependencyList, EffectCallback, useEffect, useLayoutEffect, useRef } from 'react'
+import { DependencyList, EffectCallback, useEffect, useState } from 'react'
 
 export default function useDidUpdate(fn: EffectCallback, deps: DependencyList) {
-  const isMounted = useRef<boolean>(false)
-
-  const count = useRef<number>(0)
-
-  useLayoutEffect(() => {
-    // If effect still run twice when the deps is empty array, this is strict mode, IsMounted will set to false
-    if (process.env.NODE_ENV === 'development') {
-      if (count.current > 0 && isMounted.current) {
-        isMounted.current = false
-      }
-      count.current += 1
-    }
-  }, [])
+  const [mountedFlag, setMountedFlag] = useState<boolean>(false)
 
   useEffect(() => {
-    if (!isMounted.current) {
-      isMounted.current = true
-      return
-    }
-
-    // eslint-disable-next-line consistent-return
+    if (!mountedFlag) return setMountedFlag(true)
     return fn()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps)
 }
--- src/hooks/useMount.ts
+++ src/hooks/useMount.ts
@@ -1,12 +1,7 @@
-import { EffectCallback, useEffect, useRef } from 'react'
+import { EffectCallback, useEffect } from 'react'
 
 export default function useMount(fn: EffectCallback) {
-  const firstRun = useRef<boolean>(true)
   useEffect(() => {
-    if (firstRun.current) {
-      firstRun.current = false
-      return fn()
-    }
-    return
+    return fn()
   }, [])
 }
