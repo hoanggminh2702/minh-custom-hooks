722de7ab95f45cc29a93d8bc711dd8f204d1d4c2
hoanggminh2702 <hoanggminh2702@gmail.com>
2024-12-23 14:55:16 +0700

    add use Task hook

---
+++ src/hooks/useTask.ts
@@ -0,0 +1,92 @@
+import { useCallback, useRef, useState } from 'react'
+import { v4 } from 'uuid'
+import useToggle from './useToggle'
+
+export enum EnumTaskState {
+  IDLE = 'IDLE',
+  PENDING = 'PENDING',
+  SUCCESS = 'SUCCESS',
+  FAIL = 'FAIL',
+  CANCELED = 'CANCELED',
+}
+
+type UnwrapPromise<T> = T extends Promise<infer U> ? U : T
+
+type UseTaskProps<TTask extends () => Promise<any>, TError extends any = any> = {
+  task: TTask
+
+  onSuccess?(data?: UnwrapPromise<ReturnType<TTask>>): void
+  onError?(error?: TError): void
+  onFinally?(): void
+  preserve?: boolean
+  preserveWhenError?: boolean
+}
+
+export default function useTask<TTask extends (...args: any[]) => Promise<any>, TError extends any>({
+  task,
+  onSuccess,
+  onError,
+  onFinally,
+  preserve = false,
+  preserveWhenError = false,
+}: UseTaskProps<TTask, TError>) {
+  const { state, on, off } = useToggle()
+
+  const [taskData, setTaskData] = useState<UnwrapPromise<ReturnType<TTask>>>()
+  const [taskError, setTaskError] = useState<TError>()
+  const [taskState, setTaskState] = useState<EnumTaskState>(EnumTaskState.IDLE)
+
+  // STORE NEWEST TASK ID TO MAKE SURE TASK DATA AND TASK STATE IS NEWEST
+  const newestTask = useRef<string>()
+
+  const runTask = useCallback(
+    async (...args: Parameters<typeof task>) => {
+      const taskId = v4()
+      newestTask.current = taskId
+      on()
+      setTaskState(EnumTaskState.PENDING)
+      !preserve && setTaskData(undefined)
+      try {
+        const res = await task(...args)
+        if (newestTask.current === taskId) {
+          onSuccess && onSuccess(res)
+          setTaskData(res)
+          setTaskState(EnumTaskState.SUCCESS)
+        }
+        return res
+      } catch (err: any) {
+        if (newestTask.current === taskId) {
+          onError && onError(err)
+          !preserveWhenError && setTaskData(undefined)
+          setTaskState(EnumTaskState.FAIL)
+          setTaskError(err)
+        }
+
+        throw err
+      } finally {
+        if (newestTask.current === taskId) {
+          // If newest task !== taskId, new task has been created, and it will surely reach finally and off loading would be trigger
+          // else if it is canceled, the task state and loading state is immediately set when cancel function is triggered
+          off()
+          onFinally && onFinally()
+        }
+      }
+    },
+    [task, onSuccess, onError, onFinally, preserve],
+  )
+
+  const cancelTask = useCallback(() => {
+    newestTask.current = undefined
+    off()
+    setTaskState(EnumTaskState.CANCELED)
+  }, [])
+
+  return {
+    data: taskData,
+    error: taskError,
+    state: taskState,
+    isLoading: state,
+    run: runTask,
+    cancel: cancelTask,
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import useLayoutDidUpdate from './hooks/useLayoutDidUpdate'
 import useLayoutMount from './hooks/useLayoutMount'
 import useLocalStorage from './hooks/useLocalStorage'
 import useMount from './hooks/useMount'
+import useTask from './hooks/useTask'
 import useToggle from './hooks/useToggle'
 
 export {
@@ -15,5 +16,6 @@ export {
   useLayoutMount,
   useLocalStorage,
   useMount,
+  useTask,
   useToggle,
 }
